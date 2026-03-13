5b1a1c9e2215027b5015021d2292d69dbf52f295
thevravireddy <thevravireddy@gmail.com>
2026-03-13 14:49:39 +0530

    Update main.py

--- backend/api_service/main.py
+++ backend/api_service/main.py
@@ -114,5 +114,6 @@ async def get_info(symbol: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
